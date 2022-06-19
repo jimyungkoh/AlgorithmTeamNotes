a50235d234c76ac99022304c1d768d9eaed578aa
jimyungkoh <kojimyung@tukorea.ac.kr>
2022-06-19 21:50:57 +0900

    [update] README.md, No42586.java, No42586.md

---
+++ README.md
@@ -0,0 +1,3 @@
+
+* 스택 / 큐
+  * [프로그래머스 - #42586 - 기능개발](https://github.com/jimyungkoh/algorithmTeamNotes/src/main/java/programmers/stackAndQueue/No42586.md)
--- src/main/java/programmers/stackAndQueue/No42586.java
+++ src/main/java/programmers/stackAndQueue/No42586.java
@@ -1,14 +1,15 @@
 package programmers.stackAndQueue;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 
 public class No42586 {
+
     @Test
-    private void testSolution() {
+    public void testSolutionForCase1() {
         //given
         int[] progresses = {93, 30, 55};
         int[] speeds = {1, 30, 5};
@@ -18,18 +19,50 @@ public class No42586 {
         int[] answer = new int[]{2, 1};
 
         //then
-        Assertions.assertArrayEquals(answer, result);
+        assertArrayEquals(answer, result);
+    }
+
+    @Test
+    public void testSolutionForCase2() {
+        //given
+        int[] progresses = {95, 90, 99, 99, 80, 99};
+        int[] speeds = {1, 1, 1, 1, 1, 1};
+
+        //when
+        int[] result = solution(progresses, speeds);
+        int[] answer = new int[]{1, 3, 2};
+
+        //then
+        assertArrayEquals(answer, result);
     }
 
     private int[] solution(int[] progresses, int[] speeds) {
         List<Integer> remainedDays = new ArrayList<>();
 
-        for (int idx = 0; idx<progresses.length; idx++){
-            remainedDays.add((int)Math.ceil((100-progresses[idx])/speeds[idx]));
+        for (int idx = 0; idx < progresses.length; idx++) {
+            int remainedDay = (int) Math.ceil((100 - progresses[idx]) / (double)speeds[idx]);
+            remainedDays.add(remainedDay);
         }
 
-        remainedDays.forEach(System.out::println);
+        Stack<Integer> functionStack = new Stack<>();
+        int max = Integer.MIN_VALUE;
 
-        return new int[0];
+        for (int remainedDay : remainedDays) {
+            if (remainedDay > max || functionStack.isEmpty()) {
+                max = remainedDay;
+                functionStack.push(1);
+            } else {
+                functionStack.push(functionStack.pop() + 1);
+            }
+        }
+
+        int[] functionsByDeployment = new int[functionStack.size()];
+        int functionsByDeploymentIdx = functionsByDeployment.length-1;
+
+        while (!functionStack.isEmpty()){
+            functionsByDeployment[functionsByDeploymentIdx--] = functionStack.pop();
+        }
+
+        return functionsByDeployment;
     }
 }
